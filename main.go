f5c1ae862259ea44a27d84ac61785da2ecdfb267
Marc Lambrichs <mlambrichs@melange-it.nl>
2016-07-08 08:42:39 +0200

    Cleaned up main

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-import "github.com/mlambrichs/graphite-tools/commands"
+import (
+	"github.com/mlambrichs/graphite-tools/commands"
+)
 
 func main() {
-	if err := commands.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
+	commands.Execute()
 }
